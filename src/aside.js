500f81c06ddde50cbdc8163ae4c934ef861268c0
AlainAlcantara <165932661+AlainAlcantara@users.noreply.github.com>
2024-04-24 21:22:02 +0000

    trabajito

--- src/aside.js
+++ src/aside.js
@@ -5,30 +5,36 @@ setTimeout(()=>{
 const Aside = () => {
     return <aside>
         <div className="col-md-6">
-            <h1>REGISTRATE</h1>
+            <h1>Registrate<i data-feather="smile"></i></h1>
             <form action="">
+            <div className="mb-3">
+    <label htmlFor="nombre" className="form-label">
+        <i data-feather="user"></i> Nombre
+    </label>
+    <input type="text" className="form-control" id="nombre" placeholder="" />
+</div>
                 <div className="mb-3">
-                    <label htmlFor="nombre" className="form-label">Nombre</label>
-                    <input type="text" className="form-control" id="nombre" placeholder="" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="apellidos" className="form-label">Apellidos</label>
+                    <label htmlFor="apellidos" className="form-label"> <i data-feather="user"></i> Apellidos</label>
                     <input type="text" className="form-control" id="apellidos" placeholder="" />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="direccion" className="form-label">Direccion</label>
-                    <input type="text" className="form-control" id="direccion" placeholder="" />
-                </div>
+    <label htmlFor="direccion" className="form-label">
+        <i data-feather="home"></i> Dirección
+    </label>
+    <input type="text" className="form-control" id="direccion" placeholder="" />
+</div>
+<div className="mb-3">
+    <label htmlFor="usuario" className="form-label">
+        <i data-feather="check-circle"></i> Usuario
+    </label>
+    <input type="text" className="form-control" id="usuario" placeholder="" />
+</div>
                 <div className="mb-3">
-                    <label htmlFor="usuario" className="form-label">Usuario</label>
-                    <input type="text" className="form-control" id="usuario" placeholder="" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="correo" className="form-label">Correo Electronico</label>
+                    <label htmlFor="correo" className="form-label"> <i data-feather="mail"></i>Correo Electronico</label>
                     <input type="text" className="form-control" id="correo" placeholder="" />
                 </div>
                 <div className="col-md-6">
-                    <label htmlFor="contrasena" className="form-label">Contraseña</label>
+                    <label htmlFor="contrasena" className="form-label"><i data-feather="lock"></i>Contraseña</label>
                     <input type="password" className="form-control" id="contrasena" />
                 </div>
                 <div className="col-md-4">
@@ -41,10 +47,10 @@ const Aside = () => {
                     </select>
                 </div>
                 <div className="col-12 mt-3 mb-3">
-                    <button type="submit" className="btn btn-primary">Registrar Usuario                
-                    <i data-feather="save"></i>
-                    </button>
-                </div>
+    <button type="submit" className="btn btn-primary">
+        <i data-feather="save"></i> Registrar Usuario
+    </button>
+</div>
             </form>
         </div>
     </aside>;
